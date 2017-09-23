e2570996b4361acbd51476b42eac384262369502
roserenity <calongabigail@gmail.com>
2017-09-23 14:28:12 +0800

    :D

---
+++ home.php
@@ -0,0 +1,100 @@
+ <html>
+ <head>
+ <link rel="stylesheet" type="text/css" href="mycss.css">
+  <link rel="stylesheet" type="text/css" href="homepage.css">
+  <link rel="stylesheet" type="text/css" href="/bootstrap/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="/bootstrap/bootstrap-theme.css">
+  
+
+ </head>
+ <body>
+ <nav class="navbar navbar-custom navbar-fixed-top text-center">
+					
+						<div class="navbar-header" >
+						  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>                        
+						  </button>
+					  <img src = "/img/LOGO.png" width = "100" class="img-responsive" alt="Los Angeles">
+						
+						</div>
+						
+						<div  class="collapse navbar-collapse" id="myNavbar">
+						
+						  <ul class="nav navbar-nav">
+							<li class="active"><a href="#">Home</a></li>
+							<li><a href="#">Travel</a></li>
+							<li><a href="#">History</a></li>
+							<li><a href="#">About</a></li>
+							
+						  </ul>
+						  <ul class = "nav navbar-nav " style = "margin-top: -10px;">
+						  <li> 
+								<form action="" method="get" id="search">	
+									<input type="text" name="search" class="search" value = "Search places"/> 
+									<input type="submit" value="Find" />
+								</form> 
+						  </li>
+						  </ul>
+						 
+						</div>
+</nav>
+	
+	
+	
+<div class = "container-fluid" style = "background-image: url('img/background')">	
+
+	<div class = "row text-center" id ="embed">
+	<br />
+		<div class = "col-md-6">
+			<!-- embed -->
+			<iframe width="100%" height="315" src="https://www.youtube.com/embed/ADNgEHFDYzo" frameborder="0" allowfullscreen></iframe>
+		</div>
+		<div class = "col-md-6">
+			<h1> Discover more with videos</h1>
+			<h3> It's 7,107 times more fun in the Philippines. Watch the official Philippine Department of Tourism AVP  to find out why.</h3>
+			<a href = "https://www.youtube.com/user/morefunph" > Tourism Philippines</a>
+		</div>
+	</div>
+	
+	
+	
+	
+	
+	<!-- footer -->
+	<div class = "row text-center" id = "footer">
+		<div class = "col-md-4">
+			 <img src = "<?php echo base_url('img/logo.png') ?>" width = "100%" class="img-responsive" alt="Los Angeles">
+		</div>
+		<div class = "col-md-2 text-center">
+			<h3> FOLLOW US ON</h3>
+			<a href = ""> Twitter</a>
+			<a href = ""> Facebook  </a>
+			
+		</div>
+		<div class = "col-md-2">
+			<h3> CONTACT US </h3>
+			<h4> 09152987364</h4>
+			<h4> pili_pinas@gmail.com</h4>
+			<h4> TUP Manila</h4>
+		</div>
+		<div class = "col-md-2">
+			<h3> ABOUT US</h3>
+			<h4> Sitemap</h4>
+			<h4> Privacy Policy</h4>
+			<h4> Terms and Conditions</h4>
+		</div>
+		<div class = "col-md-2">
+		</div>
+	</div>
+	
+	<div class = "row text-center" id ="ARR">
+		<h3> All Rights Reserved 2017<h3>
+	</div>
+
+	
+
+</div>
+</body>
+<html>
