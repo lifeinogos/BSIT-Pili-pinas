ef8dcbd167e5486e8cf1c3b757a5799cd83f69ac
Robilyn ^-^ <31682985+RAbenoja@users.noreply.github.com>
2017-09-28 10:36:28 +0800

    Add files via upload
    ito na ung sa about, babaguhin ko pa yan, di pa tapos

--- about.php
+++ about.php
@@ -1,5 +1,5 @@
-<!-- About page -->
-
+<!-- Homepage -->
+	  
 	 <nav class="navbar navbar-custom navbar-fixed-top text-center">
 					
 						<div class="navbar-header" >
@@ -8,124 +8,144 @@
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>                        
 						  </button>
-							<img src = "<?php echo base_url('img/logo.png') ?>" width = "100" class="img-responsive" alt="Los Angeles">
+					  <img src = "<?php echo base_url('img/logo.png') ?>" width = "100" class="img-responsive" alt="Los Angeles">
 						
 						</div>
 						
-			<div  class="collapse navbar-collapse" id="myNavbar">
+						<div  class="collapse navbar-collapse" id="myNavbar">
 						
 				<ul class="nav navbar-nav">
 							<li class="active"><a href="<?php echo base_url('Pilipinas/home/');?>">Home</a></li>
-							<li><a href="<?php echo base_url('Pilipinas/travel/');?>">Travel
-							<li><a href="<?php echo base_url('Pilipinas/history/');?>">History
-							<li><a href="<?php echo base_url('Pilipinas/about/');?>">About				
+							<li class="dropdown">
+									<a class="dropdown-toggle" data-toggle="dropdown" href="">Travel
+									<span class="caret"></span></a>
+									<ul class="dropdown-menu">
+									  <li><a href="<?php echo base_url('Pilipinas/travel/');?>"> Breathtaking Tourist Spots </a></li>
+									  <li class="divider"></li>
+									  <li><a href="<?php echo base_url('Pilipinas/travel2/');?>">Must Try Outdoor Activities</a></li>
+									  <li class="divider"></li>
+									  <li><a href="<?php echo base_url('Pilipinas/travel3/');?>">Calendar of Festivals</a></li> 
+									</ul>
+								  </li>
+						    <li class="dropdown">
+									<a class="dropdown-toggle" data-toggle="dropdown" href="">History
+									<span class="caret"></span></a>
+									<ul class="dropdown-menu">
+									  <li><a href="<?php echo base_url('Pilipinas/history/');?>">World Heritage Sites</a></li>
+									  <li class="divider"></li>
+									  <li><a href="#">Heritage Events</a></li>
+									</ul>
+								  </li>
+							 <li class="active"><a href="<?php echo base_url('Pilipinas/about/');?>">About</a></li>
 			    </ul> 
 						  
-				 <ul class = "nav navbar-nav " style = "margin-top: -10px;">
+						
+						  <ul class = "nav navbar-nav " style = "margin-top: -10px;">
 						  <li> 
 								<form action="" method="get" id="search">	
 									<input type="text" name="search" class="search" value = "Search places"/> 
 									<input type="submit" value="Search" />
 								</form> 
 						  </li>
-				 </ul>		 		 
-			</div>
+						  </ul>
+						 
+						 
+						</div>
 	</nav>
-<div class = "container-fluid" style = "background-image: url('img/background')">	
+	
 
+	<div class = "container-fluid" style = "background-image: url('img/background')">	
 	<div class = "row">
-	<br />
-	<br />
-	  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-	  
-		<!-- Indicators -->
-		<ol class="carousel-indicators">
-		  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-		  <li data-target="#myCarousel" data-slide-to="1"></li>
-		  <li data-target="#myCarousel" data-slide-to="2"></li> 
-		  <li data-target="#myCarousel" data-slide-to="3"></li>
-		  <li data-target="#myCarousel" data-slide-to="4"></li>
-		</ol>
-
-		<!-- Wrapper for slides -->
-		<div class="carousel-inner" style = "box-shadow: 0px 2px 35px black;">
-		  <div class="item active">
-			<img src="<?php echo base_url('img/slider_1.jpg') ?>" alt="welcome to philippines" style="width:100%;">
-		  </div>
-
-		  <div class="item">
-			<img src="<?php echo base_url('img/slider_2.jpg') ?>" alt="" style="width:100%;">
-		  </div>
+		 <img src = "<?php echo base_url('img/slider_1.jpg') ?>" width = "100%" height="60%" class="img-responsive" alt="Los Angeles">
+		 <img src = "<?php echo base_url('img/slider_2.jpg') ?>" width = "100%" height="60%" class="img-responsive" alt="Los Angeles">
+		 <img src = "<?php echo base_url('img/slider_3.jpg') ?>" width = "100%" height="60%" class="img-responsive" alt="Los Angeles">
+		 <img src = "<?php echo base_url('img/slider_4.jpg') ?>" width = "100%" height="60%" class="img-responsive" alt="Los Angeles">
+		 <img src = "<?php echo base_url('img/slider_5.jpg') ?>" width = "100%" height="60%" class="img-responsive" alt="Los Angeles">
 		
-		  <div class="item">
-			<img src="<?php echo base_url('img/slider_3.jpg') ?>" alt="" style="width:100%;">
-		  </div>
-		  
-		  <div class="item">
-			<img src="<?php echo base_url('img/slider_4.jpg') ?>" alt="" style="width:100%;">
-		  </div>
-		  
-		  <div class="item">
-			<img src="<?php echo base_url('img/slider_5.jpg') ?>" alt="" style="width:100%;">
-		  </div>
+	</div>
+		
+	<!--	<div class = "row text-center" id ="embed">
+		<div class = "row">
+		<div align="center"> 	
+			 <img src = "<?php echo base_url('img/main.jpg') ?>" width = "95%" height="60%" class="img-responsive" alt="Los Angeles">
+		
 		</div>
-
-		<!-- Left and right controls -->
-		<a class="left carousel-control" href="#myCarousel" data-slide="prev">
-		  <span class="glyphicon glyphicon-chevron-left"></span>
-		  <span class="sr-only">Previous</span>
-		</a>
-		<a class="right carousel-control" href="#myCarousel" data-slide="next">
-		  <span class="glyphicon glyphicon-chevron-right"></span>
-		  <span class="sr-only">Next</span>
-		</a>
 	  </div>
 	</div>
+	
+	-->
+	
+	<!--<div id ="about">
+		<div class = "col-md-6">
+			<!-- embed -->
+	<!--		<h1> What We Do</h1>
+			<h3> serves as an online tourist hub showcasing detailed information about the country’s heritage, culture, and natural landmarks.
+								
+									  
+									This website will also promote local businesses by advertising their products and services for free; like local-made souvenirs and local travel guides.</p>
+								
+									Pili-Pinas is a website that aims to attract Locals and International Tourists who wish to travel around The Philippines.
+									It also aims to advocate local businessmen in promoting Filipino-pride goods and services.
+									<br />
+									<br />
+</h3>
+			
+		</div>
+		<div class = "col-md-6">
+			<div align="center"> 
+				<br />
+			 <img src = "<?php echo base_url('img/main.jpg') ?>" width = "95%" height="60%" class="img-responsive" alt="Los Angeles">
+		
+		</div>
+		</div>
+		
+		
+	</div>
+
+
+	-->
+
+
+
+	
+	
+<div class = "row text-center" id ="about">
+	<br />
+		<div class = "col-md-6">
+			<!-- embed -->
+			<h1> What We Do</h1>
+			<h3> serves as an online tourist hub showcasing detailed information about the country’s heritage, culture, and natural landmarks.
+								
+									  
+									This website will also promote local businesses by advertising their products and services for free; like local-made souvenirs and local travel guides.</p>
+								
+									Pili-Pinas is a website that aims to attract Locals and International Tourists who wish to travel around The Philippines.
+									It also aims to advocate local businessmen in promoting Filipino-pride goods and services.
+									<br />
+									<br /><br /> <br />
+		</div>
+		<div class = "col-md-6">
+			
+			 <img src = "<?php echo base_url('img/main.jpg') ?>" width = "95%" height="60%" class="img-responsive" alt="Los Angeles">
+		
+		</div>
+	</div>
+	
+
 	
 
 	
 	<div class = "row text-center" id ="embed">
 	<br />
 		<div class = "col-md-6">
-			<img src="<?php echo base_url('img/main.jpg') ?>" alt="" style="width:100%;">
+			<!-- embed -->
+			<iframe width="100%" height="315" src="https://www.youtube.com/embed/ADNgEHFDYzo" frameborder="0" allowfullscreen></iframe>
 		</div>
-	
 		<div class = "col-md-6">
-			<h1> ABOUT US </h1>
-			<h3>PILIPINAS serves as an online tourist hub showcasing detailed information about the country’s heritage, culture, and natural landmarks.</h3>
-									  
-			<p>This website will also promote local businesses by advertising their products and services for free; like local-made souvenirs and local travel guides.
-								
-				Pili-Pinas is a website that aims to attract Locals and International Tourists who wish to travel around The Philippines.
-				It also aims to advocate local businessmen in promoting Filipino-pride goods and services.</p>
-				<br />
-				<br />
+			<h1> Discover more with videos</h1>
+			<h3> It's 7,107 times more fun in the Philippines. Watch the official Philippine Department of Tourism AVP  to find out why.</h3>
+			<a href = "https://www.youtube.com/user/morefunph" > Tourism Philippines</a>
 		</div>
-	</div>
-	
-		<div class = "col-md-6">
-		<br />
-			<img src="<?php echo base_url('img/featured_1.jpg') ?>" alt="" style="width:60%;">
-			<img src="<?php echo base_url('img/featured_3.jpg') ?>" alt="" style="width:60%;">
-		</div>
-
-		<div class = "col-md-6">
-			<h1>PILIPINAS</h1>
-			<p>has the following features:
-					<ul>
-						<li>One-stop interactive detailed website for locating all Philippine’s tourist destinations</li>
-						<li>List and galleries of places categorized accordingly</li>
-						<li>Suggestion based on filters chosen by the tourist</li>
-						<li>Integrated local travel guide offers</li>
-						<li>Voting for popularity and experience</li>
-						<li>Reviews that will help in suggestion for future development of that place</li>
-						<li>Advertisement for local products on it’s respective places</li>
-						<li>Estimated minimum budget needed for visiting the spot</li>
-					</ul>
-			
-			</p>
-		</div>
-
 	</div>
 	
 	
@@ -159,5 +179,6 @@
 	<div class = "row text-center" id ="ARR">
 		<h3> All Rights Reserved 2017<h3>
 	</div>
-</div>
 
+	
+</div>
